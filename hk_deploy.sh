ee1e7f8fec895d2260167c69deda79b12b3ea018
uk365 <126067835+uk365@users.noreply.github.com>
2024-03-22 21:23:06 +0530

    Update hk_deploy.sh

--- hk_deploy.sh
+++ hk_deploy.sh
@@ -2,7 +2,7 @@
 repo_url="https://github.com/uk365/FZBypassBot"
 repo_branch='main'
 
-if [ -z "$repo_url" ] || [ -z "$repo_branch" ]; then
+if [ -z repo_url ] || [ -z repo_branch ]; then
   echo "REPO_URL or REPO_BRANCH environment variable is not set. Exiting ..."
   exit 1
 fi
@@ -14,9 +14,9 @@ fi
 git init -q
 git config --global user.email drxxstrange@gmail.com
 git config --global user.name SilentDemonSD
-git remote add origin $repo_url
+git remote add origin repo_url
 git fetch origin -q
-git reset --hard origin/$repo_branch -q
+git reset --hard origin/repo_branch -q
 
 pip install -r requirements.txt --quiet
 
@@ -26,4 +26,4 @@ if [ -z "bash start.sh" ]; then
 fi
 
 echo "MrTamilKiK"
-eval "$start_cmd"
+eval "bash start.sh"
