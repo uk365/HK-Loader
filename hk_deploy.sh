b307e135c352b7fd2dd348884d137f20535df4cb
uk365 <126067835+uk365@users.noreply.github.com>
2024-03-22 21:26:39 +0530

    Update hk_deploy.sh

--- hk_deploy.sh
+++ hk_deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 repo_url="https://github.com/uk365/FZBypassBot"
 repo_branch='main'
 
-if [ -z repo_url ] || [ -z repo_branch ]; then
+# repo_url=$REPO_URL
+# repo_branch=$REPO_BRANCH
+
+if [ -z "$repo_url" ] || [ -z "$repo_branch" ]; then
   echo "REPO_URL or REPO_BRANCH environment variable is not set. Exiting ..."
   exit 1
 fi
@@ -14,16 +18,21 @@ fi
 git init -q
 git config --global user.email drxxstrange@gmail.com
 git config --global user.name SilentDemonSD
-git remote add origin repo_url
+git remote add origin $repo_url
 git fetch origin -q
-git reset --hard origin/repo_branch -q
+git reset --hard origin/$repo_branch -q
 
 pip install -r requirements.txt --quiet
 
-if [ -z "bash start.sh" ]; then
+start_cmd=$START_CMD
+if [ -z "$start_cmd" ]; then
   echo "START_CMD not specified. Exiting Now ..."
   exit 1
 fi
 
-echo "MrTamilKiK"
-eval "bash start.sh"
+echo "
+█░█ █▄▀ ▄▄ █░░ █▀█ ▄▀█ █▀▄ █▀▀ █▀█
+█▀█ █░█ ░░ █▄▄ █▄█ █▀█ █▄▀ ██▄ █▀▄
+                                 v1.0.0
+Repo : https://github.com/SilentDemonSD/HK-Loader By SilentDemonSD"
+eval "$start_cmd"
